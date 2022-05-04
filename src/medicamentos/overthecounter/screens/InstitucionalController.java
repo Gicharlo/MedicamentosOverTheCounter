82cd2b80592dcfb2c7b15ec94487ddf7c4ab2a67
GiCharlo <giovanna.charlo@hotmail.com>
2022-05-04 18:58:16 -0300

    Institucional

---
+++ src/medicamentos/overthecounter/screens/InstitucionalController.java
@@ -0,0 +1,20 @@
+
+package medicamentos.overthecounter.screens;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author giova
+ */
+public class InstitucionalController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
