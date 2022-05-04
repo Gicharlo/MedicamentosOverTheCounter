ed9e6a168ba4e9c7c36a0f0153cfc9a6614c63f9
GiCharlo <giovanna.charlo@hotmail.com>
2022-05-04 17:28:10 -0300

    Passagem entre duas Janelas

--- src/medicamentos/overthecounter/application/FXMLDocumentController.java
+++ src/medicamentos/overthecounter/application/FXMLDocumentController.java
@@ -1,11 +1,18 @@
 package medicamentos.overthecounter.application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class FXMLDocumentController implements Initializable {
     
@@ -14,8 +21,16 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void IniciarConsulta(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        try {
+            FXMLLoader tela2 = new FXMLLoader(getClass().getResource("InicialCliente.fxml"));
+            Parent root1 = ((Parent) tela2.load());
+            Stage stage = new Stage();
+            stage.setTitle("Inicial Cliente");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     @Override
--- src/medicamentos/overthecounter/application/Program.java
+++ src/medicamentos/overthecounter/application/Program.java
@@ -13,7 +13,7 @@ public class Program extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Descanso.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
